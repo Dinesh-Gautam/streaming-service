39c52e3dbcdcb4d66ebca6d0c32d68652508ec4a
Dinesh-Gautam <dineshgautam5252@gmail.com>
2023-02-09 21:13:53 +0530

    working on better slider

--- src/components/home/slider/Index.js
+++ src/components/home/slider/Index.js
@@ -15,12 +15,19 @@ function Slider({ title, data, setIsScrolling }) {
 
   const [prevIndex, setPrevIndex] = useState(itemsLength);
 
+  const [dataArr, setDataArr] = useState(data);
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const [nextIndex, setNextIndex] = useState(1);
 
+  const [transitionState, setTransitionState] = useState({
+    transition: "",
+    transform: "translateX(-100%)",
+  });
+
   const [disable, setDisable] = useState(false);
   const disableTimeoutRef = useRef(null);
-  function buttonClick() {
+  function buttonClick(type) {
     setDisable(true);
     clearTimeout(disableTimeoutRef.current);
 
@@ -29,10 +36,29 @@ function Slider({ title, data, setIsScrolling }) {
       // setHoverCardActive(false);
       // setAnimating(true);
       setIsScrolling(true);
+      if (type === "next") {
+        const transition = "transform 1s ease-in-out";
+        setTransitionState({ transition, transform: "translateX(-200%)" });
+      } else {
+        const transition = "transform 1s ease-in-out";
+        setTransitionState({ transition, transform: "translateX(0%)" });
+      }
       disableTimeoutRef.current = setTimeout(() => {
         setDisable(false);
         setIsScrolling(false);
         // setAnimating(false);
+        if (type === "next") {
+          setTransitionState({
+            transition: "",
+            transform: "translateX(-100%)",
+          });
+        } else {
+          setTransitionState({
+            transition: "",
+            transform: "translateX(-100%)",
+          });
+        }
+        updateDataArr(type);
         console.log("removing", disableTimeoutRef.current);
         disableTimeoutRef.current = null;
       }, 1000);
@@ -58,24 +84,64 @@ function Slider({ title, data, setIsScrolling }) {
   }
 
   const handleNext = () => {
-    buttonClick();
+    buttonClick("next");
     setNextIndex(setIndexNext);
     setCurrentIndex(setIndexNext);
     setPrevIndex(setIndexNext);
   };
 
   const handlePrev = () => {
-    buttonClick();
+    buttonClick("prev");
     setNextIndex(setIndexPrev);
     setCurrentIndex(setIndexPrev);
     setPrevIndex(setIndexPrev);
   };
 
+  function updateDataArr(type) {
+    console.log(type);
+    if (type === "next") {
+      const slicedArr0 = dataArr.slice(0, 5);
+      const slicedArr1 = dataArr.slice(5, 10);
+      const slicedArr2 = dataArr.slice(10, 15);
+      const slicedArr3 = dataArr.slice(data.length - 5, 5);
+      console.log(dataArr);
+      setDataArr([...slicedArr3, ...slicedArr1, ...slicedArr2, ...slicedArr0]);
+    } else {
+      const slicedArr0 = dataArr.slice(0, 5);
+      const slicedArr1 = dataArr.slice(5, 10);
+      const slicedArr2 = dataArr.slice(10, 15);
+      const slicedArr3 = dataArr.slice(data.length - 5, 5);
+
+      setDataArr([...slicedArr2, ...slicedArr3, ...slicedArr0, ...slicedArr1]);
+    }
+    // if (type === "next") {
+    //   const slicedArr0 = dataArr.slice(0, 5);
+    //   const slicedArr1 = dataArr.slice(5, 5);
+    //   const slicedArr2 = dataArr.slice(10, 5);
+    //   const slicedArr3 = dataArr.slice(data.length - 5, 5);
+    //   setDataArr([...slicedArr3, ...slicedArr1, ...slicedArr2, ...slicedArr0]);
+    // } else {
+    //   const slicedArr0 = dataArr.slice(0, 5);
+    //   const slicedArr1 = dataArr.slice(5, 5);
+    //   const slicedArr2 = dataArr.slice(10, data.length - 5);
+    //   const slicedArr3 = dataArr.slice(data.length - 5, 5);
+    //   setDataArr([...slicedArr2, ...slicedArr3, ...slicedArr0, slicedArr1]);
+    // }
+  }
+
   return (
     <>
       {title && <h2 style={{ marginLeft: "2rem" }}>{title}</h2>}
-      <div className={styles.container}>
-        {Array.from({ length }).map((_, index) => {
+      {data && (
+        <div className={styles.container}>
+          <div
+            style={{
+              transition: transitionState.transition,
+              transform: transitionState.transform,
+            }}
+            className={styles.wrapper}
+          >
+            {/* {Array.from({ length }).map((_, index) => {
           return (
             <motion.div
               key={index}
@@ -130,23 +196,46 @@ function Slider({ title, data, setIsScrolling }) {
               ))}
             </motion.div>
           );
-        })}
-
-        <button
-          disabled={disable}
-          className={styles.leftButton + " " + styles.btn}
-          onClick={handlePrev}
-        >
-          <ArrowForwardIosIcon style={{ transform: "rotate(-180deg)" }} />
-        </button>
-        <button
-          disabled={disable}
-          className={styles.rightButton + " " + styles.btn}
-          onClick={handleNext}
-        >
-          <ArrowForwardIosIcon />
-        </button>
-      </div>
+        })} */}
+            {dataArr.map((e, imgIndex) => (
+              <div className={styles.item} key={imgIndex}>
+                <FadeImageOnLoad
+                  imageSrc={e.backdrop_path || ""}
+                  //   ambientMode
+                  //   positionAbsolute
+                  //   ambientOptions={{ blur: 128, scale: 1 }}
+                  attr={{
+                    imageContainer: {
+                      className: styles.imageContainer,
+                      // id: "imageContainer",
+                      // "data-index": imgIndex + index + (itemsLength - 1) * index,
+                      // "data-ismiddle": currentIndex === index,
+                    },
+                    image: {
+                      height: 1300 / 2,
+                      width: 1300,
+                    },
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+          <button
+            disabled={disable}
+            className={styles.leftButton + " " + styles.btn}
+            onClick={handlePrev}
+          >
+            <ArrowForwardIosIcon style={{ transform: "rotate(-180deg)" }} />
+          </button>
+          <button
+            disabled={disable}
+            className={styles.rightButton + " " + styles.btn}
+            onClick={handleNext}
+          >
+            <ArrowForwardIosIcon />
+          </button>
+        </div>
+      )}
     </>
   );
 }
