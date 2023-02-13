02932ee66779e892ccecc0689d9d96d2f9ae0d3f
Dinesh-Gautam <dineshgautam5252@gmail.com>
2023-02-13 17:11:02 +0530

    made some changes

--- src/pages/api/static/[...file].js
+++ src/pages/api/static/[...file].js
@@ -12,10 +12,14 @@ export default async function handler(req, res) {
     // res.setHeader("Content-Type", "application/dash+xml");
     res.write(data);
     res.end();
+    return;
   } catch (e) {
     res.status(500).send({ success: false, error: "can't find path" });
     console.log("error occurred while send static file");
+    return;
   }
-
+  res.status(500);
+  res.send("Internal Server error");
+  res.end();
   return;
 }
