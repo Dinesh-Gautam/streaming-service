556496adfbfae8c93603b1ad7160877b52f81602
Dinesh Gautam <dineshgautam5252@gmail.com>
2023-03-30 14:02:44 +0530

    wokring on responsivness

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,7 +7,7 @@ export function useViewRedirect() {
     router.push(`/title/?id=` + item.id + "&type=" + item.media_type);
 }
 
-export function formatParagraph(para, wordsLimit = 50) {
+export function formatParagraph(para, wordsLimit = 40) {
   const paraArr = para.split(" ");
   if (paraArr.length > wordsLimit) paraArr.length = wordsLimit;
   else return para;
